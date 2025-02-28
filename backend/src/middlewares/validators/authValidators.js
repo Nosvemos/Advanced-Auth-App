70075ed3d43bea83cfc53d98364c25c7f16cbceb
Nosvemos <sametozturk1881@gmail.com>
2025-02-28 19:06:39 +0300

    authValidator added for security

---
+++ backend/src/middlewares/validators/authValidators.js
@@ -0,0 +1,29 @@
+import { body } from 'express-validator';
+import xss from 'xss';
+
+export const signupValidation = [
+  // Name Validation
+  body('name')
+  .trim()
+  .notEmpty().withMessage('Username is required.')
+  .matches(/^[a-zA-ZğüşıöçĞÜŞİÖÇ\s]+$/).withMessage('Name can only contain letters and spaces.')
+  .customSanitizer(value => xss(value)),
+
+  // Email Validation
+  body('email')
+  .trim()
+  .notEmpty().withMessage('Email is required.')
+  .isEmail().withMessage('Please enter a valid email address.')
+  .normalizeEmail()
+  .customSanitizer(value => xss(value)),
+
+  // Password Validation
+  body('password')
+  .trim()
+  .notEmpty().withMessage('Password is required')
+  .isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
+  .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-zğüşıöçĞÜŞİÖÇ\d@$!%*?&]{6,}$/)
+  .withMessage('Password must contain at least: 1 uppercase, 1 lowercase, 1 number and 1 special character')
+  .escape()
+  .customSanitizer(value => xss(value))
+];
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,9 +2,11 @@ import express from 'express';
 
 import { signup, login, logout } from '../controllers/authController.js'
 
+import { signupValidation } from '../middlewares/validators/authValidators.js';
+
 const router = express.Router();
 
-router.post('/signup', signup);
+router.post('/signup', signupValidation, signup);
 
 router.post('/login', login);
 
