51d4ced424b5451cd198ddeb488e6cab30acc503
Nosvemos <sametozturk1881@gmail.com>
2025-02-28 15:46:04 +0300

    MongoDb connection via Mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import { config } from 'dotenv'
+
+import { connectDb } from './lib/db.js'
+
+config();
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!'); 
+  res.send('Hello World!');
 });
 
 app.listen(3000, () => {
+  connectDb();
   console.log('Server is running on port 3000');
 });
---
+++ backend/src/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDb = async () => {
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB connected: ${db.connection.host}`);
+  } catch (error) {
+    console.log("Error connection to MongoDB: ", error.message);
+    process.exit(1);
+  }
+}
