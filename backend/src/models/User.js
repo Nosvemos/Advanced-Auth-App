5df5c2b70bc5bee6540e3d6031c542f17356432f
Nosvemos <sametozturk1881@gmail.com>
2025-02-28 16:34:47 +0300

    User model created

---
+++ backend/src/models/User.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+  lastLogin: {
+    type: Date,
+    default: Date.now
+  },
+  resetPasswordToken: String,
+  resetPasswordExpiresAt: Date,
+  verificationToken: String,
+  verificationExpiresAt: Date
+}, { timestamps: true });
+
+export const User = await mongoose.model('User', userSchema);
