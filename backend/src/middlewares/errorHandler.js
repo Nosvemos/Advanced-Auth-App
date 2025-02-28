73c58508c41f7a4e837abc6d01ef39ddf2e3ade9
Nosvemos <sametozturk1881@gmail.com>
2025-02-28 19:03:47 +0300

    Update errorHandler.js

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -1,9 +1,19 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  res.status(statusCode).json({
+  
+  const response = {
     success: false,
     message: err.message || "Internal Server Error",
-  });
+  };
+
+  if (err.errors && Array.isArray(err.errors)) {
+    response.errors = err.errors.map(e => ({
+      field: e.param,
+      message: e.msg,
+    }));
+  }
+
+  res.status(statusCode).json(response);
 };
 
 export default errorHandler;
