acd1f4694415f501a4cacbbcec6ba917f82cdfd4
Nosvemos <sametozturk1881@gmail.com>
2025-02-28 23:33:23 +0300

    Create sendEmail utility for sending emails

---
+++ backend/src/utils/sendEmail.js
@@ -0,0 +1,23 @@
+import { mailtrapClient, mailtrapSender } from '../lib/mailtrap/mailtrap.js'
+import {
+  VERIFICATION_EMAIL_TEMPLATE
+} from '../lib/mailtrap/emailTemplates.js'
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+  const recipientEmail = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: mailtrapSender,
+      to: recipientEmail,
+      subject: "Verify your email",
+      html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationToken}", verificationToken),
+      category: 'Email Verification'
+    });
+    if(!response?.success){
+      throw new Error(`Email couldn't send, please try again later.`);
+    }
+  } catch (error) {
+    throw new Error(`Error while sending verification email: ${error}`);
+  }
+};
