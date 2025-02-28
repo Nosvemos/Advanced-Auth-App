0e401432bb30cc0b42b4e9d3e4cb9229ce542e78
Nosvemos <sametozturk1881@gmail.com>
2025-02-28 16:34:31 +0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,27 @@
 import express from 'express';
-import { config } from 'dotenv'
+import dotenv from 'dotenv'
 
 import { connectDb } from './lib/db.js'
 
 import authRoutes from './routes/authRoutes.js'
 
-config();
+dotenv.config();
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const port = process.env.PORT || 5000;
 
 app.use('/api/auth', authRoutes)
 
-app.listen(3000, () => {
-  connectDb();
-  console.log('Server is running on port 3000');
-});
+const startServer = async () => {
+  try {
+    await connectDb();
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
