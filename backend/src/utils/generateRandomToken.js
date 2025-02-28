09286e1af4de362e43961266706a6c03bc0c9fad
Nosvemos <sametozturk1881@gmail.com>
2025-02-28 23:16:09 +0300

    Update generateRandomToken.js

--- backend/src/utils/generateRandomToken.js
+++ backend/src/utils/generateRandomToken.js
@@ -1,7 +1,8 @@
 import crypto from 'crypto';
 
-const generateRandomToken = (length= 20) => {
-  return crypto.randomBytes(length).toString('hex').slice(0, length);
+const generateRandomToken = (length= 20, uppercase = true) => {
+  let token = crypto.randomBytes(length).toString('hex').slice(0, length);
+  return uppercase ? token.toUpperCase() : token;
 };
 
 export default generateRandomToken;
