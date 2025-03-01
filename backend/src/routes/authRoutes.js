39b30fa0c272e2e11e6c03caa24b14a52492429b
Nosvemos <sametozturk1881@gmail.com>
2025-03-01 15:58:09 +0300

    Add rateLimiter

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,4 +1,11 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // 5 requests per each IP
+  message: 'Too many attempts, please try again later.'
+});
 
 import { signup, login, logout, verifyEmail, forgotPassword, resetPassword, checkAuth } from '../controllers/authController.js'
 
@@ -9,17 +16,17 @@ import { verifyToken } from '../middlewares/verifyToken.js'
 
 const router = express.Router();
 
-router.post('/signup', signupValidation, signup);
+router.post('/signup', authLimiter, signupValidation, signup);
 
-router.post('/login', loginValidation, login);
+router.post('/login', authLimiter, loginValidation, login);
 
 router.post('/logout', logout);
 
-router.post('/verify-email', verifyEmailValidation, verifyEmail);
+router.post('/verify-email', authLimiter, verifyEmailValidation, verifyEmail);
 
-router.post('/forgot-password', forgotPasswordValidation, forgotPassword);
+router.post('/forgot-password', authLimiter, forgotPasswordValidation, forgotPassword);
 
-router.post('/reset-password/:token', resetPasswordValidation, resetPassword);
+router.post('/reset-password/:token', authLimiter, resetPasswordValidation, resetPassword);
 
 router.get('/check-auth', verifyToken, checkAuth)
 
