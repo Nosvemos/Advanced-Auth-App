1eca45b1906ba757f0ac3c048736141098497190
Nosvemos <sametozturk1881@gmail.com>
2025-03-02 19:40:34 +0300

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,19 @@
+import React from 'react'
+
+import { useAuthStore } from "../store/AuthStore";
+
 const HomePage = () => {
+  const { user, logout } = useAuthStore();
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
-    <div></div>
+    <div className='flex items-center justify-center p-4'>
+      <span className='text-center font-bold'>You are successfully logged in.</span>
+      <button className="btn btn-base btn-outline rounded-md shadow-xl" onClick={handleLogout}>Logout</button>
+    </div>
   )
 }
 
